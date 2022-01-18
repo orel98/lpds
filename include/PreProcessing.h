24d2282b99c3d47991c3207a835eefd3d4802e82
orel98 <35419161+orel98@users.noreply.github.com>
2022-01-18 22:03:01 +0200

    Add files via upload

---
+++ include/PreProcessing.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "pch.h"
+
+constexpr const char *CASCADE_FILE_PATH = "res/cascades/cascade.xml";
+constexpr const double CASCADE_SCALE_FACTOR = 1.1;
+constexpr const int CASCADE_MIN_NEIGHBORS = 3;
+constexpr const int CASCADE_FLAGS = 0;
+
+// ?
+const cv::Size CASCADE_MIN_DETECTION_SIZE(53, 13);
+
+// img resize factor set to 500 pixles wide
+constexpr const int RESIZE_FACTOR = 500;
+
+void cascadeInit();
+
+cv::Mat preProcessing(const cv::Mat &_frame);
+cv::Mat cascadeDetect(const cv::Mat &_frame);
+cv::Rect operator*(cv::Rect &r, double ratio);
