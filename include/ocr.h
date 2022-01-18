7baa0a2850d2c3837927ced6fb0781383279804c
orel98 <35419161+orel98@users.noreply.github.com>
2022-01-18 22:02:26 +0200

    Create ocr.h

---
+++ include/ocr.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "pch.h"
+
+typedef tesseract::TessBaseAPI tessApi;
+//
+void ocrInit();
+void ocrCleanUp();
+const char *getTextFromImage(const cv::Mat &frame);
+
+constexpr int BYTES_PER_PIXLE = 1;
+
+constexpr const char *USER_DEFINED_DPI = "user_defined_dpi";
+constexpr const char *DPI_VALUE = "300";
+constexpr const char *TESSEDIT_CHAR_WHITELIST = "tessedit_char_whitelist";
+constexpr const char *CHARS_WHITE_LIST = "0123456789";
+
